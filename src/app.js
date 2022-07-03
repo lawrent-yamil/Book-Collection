c453b3021c20532679d1a3998e8863741a173225
novasuperior <lawrentvoli@gmail.com>
2022-07-03 12:54:34 -0600

    Updating folders and adding the .gitignore

---
+++ src/app.js
@@ -0,0 +1,107 @@
+//Definicion de Las Clases
+class Libro {
+  constructor(titulo, autor, isbn) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  static mostrarLibros() {
+    const libros = Datos.traerLibros();
+    libros.forEach((libro) => UI.agregarLibroLista(libro));
+  }
+
+  static agregarLibroLista(libro) {
+    const lista = document.querySelector("#libro-list");
+
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+            <td>${libro.titulo}</td>
+            <td>${libro.autor}</td>
+            <td>${libro.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+    lista.appendChild(fila);
+  }
+
+  static eliminarLibro(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+  static mostrarAlerta(mensaje, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(mensaje));
+
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#libro-form");
+    container.insertBefore(div, form);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
+
+  static limpiarCampos() {
+    document.querySelector("#titulo").value = "";
+    document.querySelector("#autor").value = "";
+    document.querySelector("#isbn").value = "";
+  }
+}
+
+class Datos {
+  static traerLibros() {
+    let libros;
+    if (localStorage.getItem("libros") === null) {
+      libros = [];
+    } else {
+      libros = JSON.parse(localStorage.getItem("libros"));
+    }
+    return libros;
+  }
+
+  static agregarLibro(libro) {
+    const libros = Datos.traerLibros();
+    libros.push(libro);
+    localStorage.setItem("libros", JSON.stringify(libros));
+  }
+
+  static removerLibro(isbn) {
+    const libros = Datos.traerLibros();
+    console.log(isbn);
+    libros.forEach((libro, index) => {
+      if (libro.isbn === isbn) {
+        libros.splice(index, 1);
+      }
+    });
+    localStorage.setItem("libros", JSON.stringify(libros));
+  }
+}
+
+document.addEventListener("DOMContentLoader", UI.mostrarLibros());
+//Controlar en evento submit
+document.querySelector("#libro-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //Obtener los valors de los campos
+  const titulo = document.querySelector("#titulo").value;
+  const autor = document.querySelector("#autor").value;
+  const isbn = document.querySelector("#isbn").value;
+  if (titulo === "" || autor === "" || isbn === "") {
+    UI.mostrarAlerta("Por favor Ingrese los Datos", "danger");
+  } else {
+    const libro = new Libro(titulo, autor, isbn);
+    Datos.agregarLibro(libro);
+    UI.agregarLibroLista(libro);
+    UI.mostrarAlerta("Libro Agregado a la  coleccion", "success");
+    UI.limpiarCampos();
+  }
+});
+
+document.querySelector("#libro-list").addEventListener("click", (e) => {
+  UI.eliminarLibro(e.target);
+  Datos.removerLibro(e.target.parentElement.previousElementSibling.textContent);
+  UI.mostrarAlerta("Libro Eliminado", "success");
+});
